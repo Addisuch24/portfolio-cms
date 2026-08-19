98f401c9773a4a161826f3f2497a1374c9a35d26
Addisu <edrishusesa@gmail.com>
2026-08-19 18:13:34 +0300

    Connect frontend to Railway backend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,24 +1,21 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: `${import.meta.env.VITE_API_URL}/api`
 });
 
-api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem("token");
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
 
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-
-        return config;
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
     }
-);
+
+    return config;
+});
 
 api.interceptors.response.use(
     response => response,
-
     error => {
         if (error.response?.status === 401) {
             console.warn(
